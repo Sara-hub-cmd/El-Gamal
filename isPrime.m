7f933d1846c52da84ed581f0957ef5fc5e9c501e
Osoldier <thomas.ibanez33@gmail.com>
2016-12-06 16:17:48 +0100

    Added primality test, closes #1

---
+++ isPrime.m
@@ -0,0 +1,21 @@
+function [ prime ] = isPrime( n, k )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    if(n == 2)
+        prime = 1;
+        return;
+    end
+    if(n == 1 || modulo(n,2) == 0)
+        prime = 0;
+        return;
+    end
+    for i=1:k
+        a = floor(rand()*(n-4)+2);
+        if(millerTest(a, n))
+            prime = 0;
+            return;
+        end
+    end
+    prime = 1;
+end
+
---
+++ millerTest.m
@@ -0,0 +1,29 @@
+function [ pass ] = millerTest( a,n )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    %find n-1 = 2^s * d
+    for i=0:floor(log2(n-1))
+        if(modulo((n-1), (2.^i)) == 0)
+            s = i;
+            d = (n-1)/(2.^i);
+        end
+    end
+    %apply miller test
+
+    x = modExp(a, d, n);
+    if((x == 1) || (x == n-1))
+        pass = 0;
+        return;
+    end
+    
+    while(s > 1) 
+        x = modulo(x.^2, n);
+        if(x == n-1)
+            pass = 0;
+            return;
+        end
+        s = s-1;
+    end
+    pass = 1;
+end
+
